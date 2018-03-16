d3cd9c229727621e523e1148c273cbef8e982746
IgorSavchenko <goldsav90@gmail.com>
2018-03-16 10:00:40 +0200

    Исправил баг при закрывании главного меню

--- js/main-menu.js
+++ js/main-menu.js
@@ -10,9 +10,21 @@
 		dropMenu[menuSelect.value].classList.toggle('drop-menu--show');
 	};
 
+	var closeDropMenu = function () {
+		if (dropMenu[menuSelect.value].classList.contains('drop-menu--show')) {
+			dropMenu[menuSelect.value].classList.remove('drop-menu--show');
+		}
+	};
+
 	menuToggle.addEventListener('click', function(evt) {
 		evt.preventDefault();
 		toggleDropMenu();
 	});
+
+	window.onresize = function () {
+		if (document.documentElement.clientWidth > 999) {
+			closeDropMenu();
+		}
+	};
 
 })();
